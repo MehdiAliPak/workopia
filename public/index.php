9f895951a1b97580da68eabf421aa7ac3ce40ab1
mahdi_ali <pakpak1251810@gmail.com>
2024-08-19 14:55:20 +0330

    add: routes and needed files

--- public/index.php
+++ public/index.php
@@ -1,4 +1,19 @@
 <?php
 require '../helpers.php';
 
-loadView('home');
+$routes = [
+    '/'                 => 'controllers/home.php',
+    '/listings'         => 'controllers/listings/index.php',
+    '/listings/create'  => 'controllers/listings/create.php',
+    '404'               => 'controllers/error/404.php'
+];
+
+$uri = $_SERVER['REQUEST_URI'];
+$uri = str_replace('/workopia', '', $uri);
+// inspectAndDie($uri);
+
+if (array_key_exists($uri, $routes)) {
+    require(basePath($routes[$uri]));
+} else {
+    require basePath($routes['404']);
+}
--- views/home.view.php
+++ views/home.view.php
@@ -163,7 +163,7 @@
                 </div>
             </div>
         </div>
-        <a href="listings.html" class="block text-xl text-center">
+        <a href="/workopia/listings" class="block text-xl text-center">
             <i class="fa fa-arrow-alt-circle-right"></i>
             Show All Jobs
         </a>
