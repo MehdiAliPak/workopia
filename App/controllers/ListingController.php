a4dbef05b7a76ad268f6303c9a62e9e6ad86afe0
mahdi_ali <pakpak1251810@gmail.com>
2024-09-09 17:44:30 +0330

    add: update function

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -113,7 +113,7 @@ class ListingController
             //Reload view with errors
             loadView('listings/create', [
                 'errors' => $errors,
-                'listings' => $newListingData
+                'listing' => $newListingData
             ]);
         } else {
             // Submit data
@@ -172,5 +172,115 @@ class ListingController
         $_SESSION['success_message'] = 'Listing deleted successfully';
 
         redirect('/listings');
+    }
+
+    /**
+     * Show the listing edit form
+     *
+     * @param array $params
+     * @return void
+     */
+    public function edit($params)
+    {
+        $id = $params['id'] ?? '';
+
+        $params = [
+            'id' => $id
+        ];
+
+        $listing = $this->db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+
+        // Check if listing exists
+        if (!$listing) {
+            ErrorController::notFound('Listing not found');
+            return;
+        }
+
+        loadView('listings/edit', [
+            'listing' => $listing
+        ]);
+    }
+
+    /**
+     * Update a listing
+     * 
+     * @param array $params
+     * @return void
+     */
+    public function update($params)
+    {
+        $id = $params['id'] ?? '';
+
+        $params = [
+            'id' => $id
+        ];
+
+        $listing = $this->db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+
+        // Check if listing exists
+        if (!$listing) {
+            ErrorController::notFound('Listing not found');
+            return;
+        }
+
+        $allowedFields = [
+            'title',
+            'description',
+            'salary',
+            'requirements',
+            'benefits',
+            'company',
+            'address',
+            'city',
+            'state',
+            'phone',
+            'email',
+            'tags'
+        ];
+
+        $updateValues = [];
+
+        $updateValues = array_intersect_key($_POST, array_flip($allowedFields));
+
+        $updateValues = array_map('sanitize', $updateValues);
+
+        $requiredFields = ['title', 'description', 'email', 'city', 'state', 'salary'];
+        
+        $errors = [];
+
+        foreach ($requiredFields as $field ) {
+            if(empty($updateValues[$field]) || !Validation::string($updateValues[$field])) {
+                $errors[$field] = ucfirst($field) . ' is required';
+            }
+        }
+
+        if (!empty($errors)) {
+            loadView('listings/edit', [
+                'listing' => $listing,
+                'errors' => $errors
+            ]);
+            exit;
+        } else {
+            // Submit to database
+            $updateFields = [];
+
+            foreach (array_keys($updateValues) as $field) {
+                $updateFields[] = "{$field} = :{$field}";
+            }
+            
+            $updateFields = implode(', ', $updateFields);
+            
+            
+            $updateQuery = "UPDATE listings SET $updateFields WHERE id = :id";
+
+            $updateValues['id'] = $id;
+            
+            $this->db->query($updateQuery, $updateValues);
+
+            $_SESSION['success_message'] = 'Listing Updated';
+
+            redirect('/listings/' . $id);
+        }
+
     }
 }
--- App/views/listings/edit.view.php
+++ App/views/listings/edit.view.php
@@ -2,10 +2,9 @@
 <?php loadPartial('navbar'); ?>
 <?php loadPartial('top-banner'); ?>
 
-<!-- Post a Job Form Box -->
 <section class="flex justify-center items-center mt-20">
     <div class="bg-white p-8 rounded-lg shadow-md w-full md:w-600 mx-6">
-        <h2 class="text-4xl text-center font-bold mb-4">Create Job Listing</h2>
+        <h2 class="text-4xl text-center font-bold mb-4">Edit Job Listing</h2>
         <?php if (isset($errors)) : ?>
             <?php foreach ($errors as $error) : ?>
                 <div class="message bg-red-100 my-3">
@@ -13,7 +12,8 @@
                 </div>
             <?php endforeach; ?>
         <?php endif; ?>
-        <form method="POST" action="/listings">
+        <form method="POST" action="/listings/<?= $listing->id ?>">
+            <input type="hidden" name="_method" value="PUT">
             <h2 class="text-2xl font-bold mb-6 text-center text-gray-500">
                 Job Info
             </h2>
@@ -22,21 +22,21 @@
                     type="text"
                     name="title"
                     placeholder="Job Title"
-                    value="<?= $listing['title'] ?? '' ?>"
+                    value="<?= $listing->title ?? '' ?>"
                     class="w-full px-4 py-2 border rounded focus:outline-none" />
             </div>
             <div class="mb-4">
                 <textarea
                     name="description"
                     placeholder="Job Description"
-                    class="w-full px-4 py-2 border rounded focus:outline-none"><?= $listing['description'] ?? '' ?></textarea>
+                    class="w-full px-4 py-2 border rounded focus:outline-none"><?= $listing->description ?? '' ?></textarea>
             </div>
             <div class="mb-4">
                 <input
                     type="text"
                     name="salary"
                     placeholder="Annual Salary"
-                    value="<?= $listing['salary'] ?? '' ?>"
+                    value="<?= $listing->salary ?? '' ?>"
                     class="w-full px-4 py-2 border rounded focus:outline-none" />
             </div>
             <div class="mb-4">
@@ -44,7 +44,7 @@
                     type="text"
                     name="requirements"
                     placeholder="Requirements"
-                    value="<?= $listing['requirements'] ?? '' ?>"
+                    value="<?= $listing->requirements ?? '' ?>"
                     class="w-full px-4 py-2 border rounded focus:outline-none" />
             </div>
             <div class="mb-4">
@@ -52,7 +52,7 @@
                     type="text"
                     name="tags"
                     placeholder="Tags"
-                    value="<?= $listing['tags'] ?? '' ?>"
+                    value="<?= $listing->tags ?? '' ?>"
                     class="w-full px-4 py-2 border rounded focus:outline-none" />
             </div>
             <div class="mb-4">
@@ -60,7 +60,7 @@
                     type="text"
                     name="benefits"
                     placeholder="Benefits"
-                    value="<?= $listing['benefits'] ?? '' ?>"
+                    value="<?= $listing->benefits ?? '' ?>"
                     class="w-full px-4 py-2 border rounded focus:outline-none" />
             </div>
             <h2 class="text-2xl font-bold mb-6 text-center text-gray-500">
@@ -71,7 +71,7 @@
                     type="text"
                     name="company"
                     placeholder="Company Name"
-                    value="<?= $listing['company'] ?? '' ?>"
+                    value="<?= $listing->company ?? '' ?>"
                     class="w-full px-4 py-2 border rounded focus:outline-none" />
             </div>
             <div class="mb-4">
@@ -79,7 +79,7 @@
                     type="text"
                     name="address"
                     placeholder="Address"
-                    value="<?= $listing['address'] ?? '' ?>"
+                    value="<?= $listing->address ?? '' ?>"
                     class="w-full px-4 py-2 border rounded focus:outline-none" />
             </div>
             <div class="mb-4">
@@ -87,7 +87,7 @@
                     type="text"
                     name="city"
                     placeholder="City"
-                    value="<?= $listing['city'] ?? '' ?>"
+                    value="<?= $listing->city ?? '' ?>"
                     class="w-full px-4 py-2 border rounded focus:outline-none" />
             </div>
             <div class="mb-4">
@@ -95,7 +95,7 @@
                     type="text"
                     name="state"
                     placeholder="State"
-                    value="<?= $listing['state'] ?? '' ?>"
+                    value="<?= $listing->state ?? '' ?>"
                     class="w-full px-4 py-2 border rounded focus:outline-none" />
             </div>
             <div class="mb-4">
@@ -103,7 +103,7 @@
                     type="text"
                     name="phone"
                     placeholder="Phone"
-                    value="<?= $listing['phone'] ?? '' ?>"
+                    value="<?= $listing->phone ?? '' ?>"
                     class="w-full px-4 py-2 border rounded focus:outline-none" />
             </div>
             <div class="mb-4">
@@ -111,7 +111,7 @@
                     type="email"
                     name="email"
                     placeholder="Email Address For Applications"
-                    value="<?= $listing['email'] ?? '' ?>"
+                    value="<?= $listing->email ?? '' ?>"
                     class="w-full px-4 py-2 border rounded focus:outline-none" />
             </div>
             <button
@@ -119,7 +119,7 @@
                 Save
             </button>
             <a
-                href="/"
+                href="/listings/<?= $listing->id ?>"
                 class="block text-center w-full bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded focus:outline-none">
                 Cancel
             </a>
